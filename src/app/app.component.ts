f45c1561fd2b89c6debb5793040ad8d3af35e8de
Yoann Déprez <deprez.yoann@gmail.com>
2017-01-23 16:21:47 +0100

    intégration partie 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import './rxjs-operators';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  altImg = 'Teach&Learn';
+export class AppComponent  implements OnInit {
+
+  constructor(private el:ElementRef) { }
+
+  ngOnInit() {
+  }
+
 }
+
--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+
+declare var $: any;
+
 
 @Component({
   selector: 'app-presentation',
@@ -7,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresentationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private el:ElementRef) { }
 
   ngOnInit() {
   }
